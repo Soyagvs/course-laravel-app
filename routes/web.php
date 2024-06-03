139ef54e01e25c65a0248744899bd9c8d13769b0
Soyagvs <agustinmartinezyt@gmail.com>
2024-06-02 21:31:27 -0300

    add routes the views

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'index')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/services', 'services')->name('services');
 
 
 
