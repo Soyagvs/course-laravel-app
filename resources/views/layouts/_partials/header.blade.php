47d89db44e820d03461bd48e09ccb94c6de6afad
Soyagvs <agustinmartinezyt@gmail.com>
2024-06-02 21:31:17 -0300

    create partials header

---
+++ resources/views/layouts/_partials/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+    <nav>
+        <ul>
+            <li>
+                <a href="{{route('home')}}">Home</a>
+            </li>
+            <li>
+                <a href="{{route('about')}}">About</a>
+            </li>
+            <li>
+                <a href="{{route('services')}}">Services</a>
+            </li>
+        </ul>
+    </nav>
+</header>
