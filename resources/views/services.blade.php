3baee4bf507c3888a711b0a9629334e899db4edb
Soyagvs <agustinmartinezyt@gmail.com>
2024-06-02 21:30:49 -0300

    create view services

---
+++ resources/views/services.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.layout')
+
+@section('title', 'Laravel | Services')
+
+@section('content')
+    <h1>Estos son mis servicios</h1>
+    <div class="services-container">
+    @component('_components.card')
+        @slot('title', 'Service 1')
+        @slot('content', 'services the educations and projects')
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Service 2')
+        @slot('content', 'services the educations and projects')
+    @endcomponent
+    @component('_components.card')
+        @slot('title', 'Service 3')
+        @slot('content', 'services the educations and projects')
+    @endcomponent
+    </div>
+@endsection
