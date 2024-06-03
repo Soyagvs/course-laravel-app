afe7dc6710225f10cb6538a68fb49076c54eb502
Soyagvs <agustinmartinezyt@gmail.com>
2024-06-03 12:14:14 -0300

    indexado

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -5,17 +5,19 @@
 @section('content')
     <h1 class="title-services">Estos son mis servicios</h1>
     <div class="services-container">
-    @component('_components.card')
-        @slot('title', 'Service 1')
-        @slot('content', 'services the educations and projects')
-    @endcomponent
-    @component('_components.card')
-        @slot('title', 'Service 2')
-        @slot('content', 'services the educations and projects')
-    @endcomponent
-    @component('_components.card')
-        @slot('title', 'Service 3')
-        @slot('content', 'services the educations and projects')
-    @endcomponent
+        @component('_components.card')
+            @slot('title', 'Service 1')
+            @slot('content', 'services the educations and projects')
+        @endcomponent
+
+        @component('_components.card')
+            @slot('title', 'Service 2')
+            @slot('content', 'services the educations and projects')
+        @endcomponent
+
+        @component('_components.card')
+            @slot('title', 'Service 3')
+            @slot('content', 'services the educations and projects')
+        @endcomponent
     </div>
 @endsection
